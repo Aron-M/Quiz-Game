140be746942612cdf73a6d27d607a09f149b0682
Aron-M <aron.moelich88@gmail.com>
2022-10-16 15:34:47 +0000

    media queries for full screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 const question = document.getElementById('question-block');
-const choices = document.getElementsByClassName('choice-text');
-console.log(choices);
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = true ;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
