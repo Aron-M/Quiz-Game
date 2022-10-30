a71ed30a61717d846276efa1a26c15d0c52c2042
Aron-M <aron.moelich88@gmail.com>
2022-10-30 16:07:38 +0000

    new questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,39 @@
 const QUESTIONS = [
-    {
+{
     'question': "What is 2 + 2 ??",
     "option1":"2",
     "option2": "4",
     "option3": "6",
     "correctAnswer": "4"
     },
-    {
-    'question': "in which continent is Japan?",
-    "option1":"Europe",
-    "option2": "Asia",
-    "option3": "America",
-    "correctAnswer": "Asia"
-    }
+
+{
+    'question': "What is 5 + 7 ??",
+    "option1":"6",
+    "option2": "31",
+    "option3": "12",
+    "correctAnswer": "12"
+    },
+
+{
+    'question': "What is 15 + 27 ??",
+    "option1":"36",
+    "option2": "315",
+    "option3": "42",
+    "correctAnswer": "42"
+    },
+
+{
+    'question': "What is 51 + 117 ??",
+    "option1":"168",
+    "option2": "231",
+    "option3": "112",
+    "correctAnswer": "168"
+      },
+
     ]
+
+
     let questionIndex = 0;
 
     function displayQuestion() {
@@ -24,13 +44,10 @@ const QUESTIONS = [
     }
     displayQuestion();
 
-    function submitAnswer() {
+    function submitAnswer() { 
     }
     function checkAnswer() {
     }
     function endQuiz() {
     }
    
-
-    /*
-    document.getElementById('submit').addHandler("click", submitAnswer)*/
